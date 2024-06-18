Fix bug in kalman.py check_convergence which mistakenly compared the posterior estimate covariance to Sigma_infty instead of the prior

kalman.py
@@ -142,21 +142,28 @@ class SteadyStateKalmanFilter:
             Frobenius distance between K_i and K_infty at each time step
         M_infty_dist : np.ndarray, shape (num_steps,)
             Frobenius distance between M_i and M_infty at each time step
+        Sigmas : np.ndarray, shape (num_steps, n, n)
+            The values of Sigma_i for each i
+        Ks : np.ndarray, shape (num_steps, n, m)
+            The values of K_i for each i
+        Ms : np.ndarray, shape (num_steps, n, n)
+            The values of M_i for each i
 
         Raises
         ------
         LinAlgError
             If inversion of the innovation covariance matrix fails.
         """
-        Sigmas = np.zeros((self.process.num_steps, self.process.n, self.process.n)) # Sigmas[i] = Sigma_{i|i}
+        Sigmas = np.zeros((self.process.num_steps, self.process.n, self.process.n)) # Sigmas[i] = Sigma_{i|0...i-1}
+        Sigmas_post = np.zeros((self.process.num_steps, self.process.n, self.process.n)) # Sigmas_post[i] = Sigma_{i|0...i}
         Ks = np.zeros((self.process.num_steps, self.process.n, self.process.m)) # Ks[i] = K_i
         Ms = np.zeros((self.process.num_steps, self.process.n, self.process.n)) # Ms[i] = M_i
-        Sigmas[-1] = np.zeros((self.process.n, self.process.n)) # Strictly speaking, not necessary since it's already initialized to zero
+        Sigmas_post[-1] = np.zeros((self.process.n, self.process.n)) # Strictly speaking, not necessary since it's already initialized to zero
         for i in range(self.process.num_steps):
-            Sigma_i_prior = self.process.A @ Sigmas[i-1] @ self.process.A.T + self.process.Sigma_process # Sigma_{i|i-1}
-            S_i = self.process.O @ Sigma_i_prior @ self.process.O.T + self.process.Sigma_obs # Innovation covariance
-            Ks[i] = Sigma_i_prior @ self.process.O.T @ np.linalg.inv(S_i)
-            Sigmas[i] = Sigma_i_prior - Ks[i] @ self.process.O @ Sigma_i_prior
+            Sigmas[i] = self.process.A @ Sigmas_post[i-1] @ self.process.A.T + self.process.Sigma_process
+            S_i = self.process.O @ Sigmas[i] @ self.process.O.T + self.process.Sigma_obs # Innovation covariance
+            Ks[i] = Sigmas[i] @ self.process.O.T @ np.linalg.inv(S_i)
+            Sigmas_post[i] = Sigmas[i] - Ks[i] @ self.process.O @ Sigmas[i]
             Ms[i] = self.process.A - Ks[i] @ self.process.O @ self.process.A
         
         Sigma_infty_dist = np.zeros(self.process.num_steps)
@@ -167,4 +174,4 @@ class SteadyStateKalmanFilter:
             K_infty_dist[i] = np.linalg.norm(Ks[i] - self.K_infty, ord='fro')
             M_infty_dist[i] = np.linalg.norm(Ms[i] - self.M_infty, ord='fro')
         
-        return Sigma_infty_dist, K_infty_dist, M_infty_dist
+        return Sigma_infty_dist, K_infty_dist, M_infty_dist, Sigmas, Ks, Ms